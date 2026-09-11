abf86f4456af96e17b1f3db00a1cd4b44e8709d5
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-09-11 11:20:39 +0300

    [TASK-1] Ready calculator

--- makar.lavrentev/task-1/cmd/service/main.go
+++ makar.lavrentev/task-1/cmd/service/main.go
@@ -1,1 +1,44 @@
-package service
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand", err)
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand", err)
+		return
+	}
+
+	var operation byte
+	_, err = fmt.Scanf("%c", &operation)
+
+	if b == 0 && operation == '/' {
+		fmt.Println("Division by zero", err)
+		return
+	}
+
+	var ans int
+	switch operation {
+	case '+':
+		ans = a + b
+	case '-':
+		ans = a - b
+	case '*':
+		ans = a * b
+	case '/':
+		ans = a / b
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(ans)
+}
